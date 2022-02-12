45972f3ad46f2b2479d78a6a698edc12d02078be
Mundurujoana <mundujoana@gmail.com>
2022-02-12 17:30:10 +0300

    initial commit

---
+++ src/main/kotlin/assignment1.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    val sTudentName = "Joana  Munduru"
+    println(sTudentName)
+
+    val sTudentAge = 30
+    println(sTudentAge)
+
+    var weight  = 75.5
+    println(weight)
+
+    val phoneNumb = "0753489877"
+    println(phoneNumb)
+
+        val kenyan = true
+        println(kenyan)
+        val not = false
+        println(not)
+
+}
